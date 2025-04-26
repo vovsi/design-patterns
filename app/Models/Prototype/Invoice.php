b4bbff787e5a836bbde5b2ade5e15169f789de2a
ovsi <vladovsienko0@gmail.com>
2025-04-26 09:10:30 +0300

    Add pattern Prototype.

---
+++ app/Models/Prototype/Invoice.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Prototype;
+
+class Invoice
+{
+    public \DateTime $datetime;
+
+    public function __construct(
+        public string $buyerName,
+        public string $companyName,
+        public float $total
+    )
+    {
+        $this->datetime = new \DateTime();
+    }
+
+    public function __clone(): void
+    {
+        $this->datetime = clone $this->datetime;
+    }
+}
